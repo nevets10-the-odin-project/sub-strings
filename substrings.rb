d4d0c4953ad7c4819bfc00cbf41440c11061ae55
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-12 19:27:25 -0800

    Add base looping and conditional

--- substrings.rb
+++ substrings.rb
@@ -1,3 +1,16 @@
 def substrings (string, dictionary)
-
+  string_array = string.split
+  result_hash = {}
+  
+  string_array.each do |og_string|
+    dictionary.each do |word|
+        if og_string.downcase.include?(word.downcase) then
+          puts "'#{word}' is in '#{og_string}'"
+        end
+    end
+  end
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
